e1072575370dc7409c1398134f9a696edea33225
Lyzerg <31682095+Lyzerg@users.noreply.github.com>
2017-09-06 12:11:13 -0500

    FirstUpload

---
+++ Assets/Scripts/testScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Hello World from GitHub!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
